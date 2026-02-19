da37e960aadfed9f313a7f8a393c150b55db0188
divyaraj25 <divyaraj.makwana9425@gmail.com>
2026-02-19 15:09:56 +0530

    updated 2 finish

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,6 @@ const router = express.Router();
 const User = require('../models/users');
 const { login, register } = require('../controllers/auth');
 
-router.get('/', function(req, res, next) {
-  res.status(200).json({
-    success: true,
-    data: null,
-    message: 'respond with a resource'
-  });
-});
-
 router.post('/login', login);
 router.post('/register', register);
 
